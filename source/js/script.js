9cb31b5e4a96543e559bc9c02d4405eefb3031e0
pervakam <52745442+pervakam@users.noreply.github.com>
2020-06-25 18:20:50 +0300

    исправление замечаний и критерии

--- source/js/script.js
+++ source/js/script.js
@@ -1,56 +1,22 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+var button = document.querySelector('.page-header__toggle');
+var menu = document.querySelector('.page-header__nav-wrapper');
+var logo = document.querySelector('.page-header__logo-img');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
-});
+button.classList.add('page-header__toggle--open');
+menu.classList.add('page-header__closed');
+logo.classList.add('page-header__logo--white');
+
+var closeMenu = function () {
+  menu.classList.toggle('page-header__closed');
+  button.classList.toggle('page-header__toggle--close');
+  button.classList.toggle('page-header__toggle--open');
+  logo.classList.toggle('page-header__logo--white');
+};
+
+
+button.addEventListener('click', closeMenu);
 
 
 
-
-// 'use strict';
-//
-// var menuButton = document.querySelector('.main-nav__popup-toggle');
-// var button = document.querySelector('.main-nav__toggle');
-// var menu = document.querySelector('.main-nav__list');
-// var buttonBuyTour = document.querySelectorAll('.buy-tour');
-// var buyTourPopup = document.querySelector('.buy-tour-popup');
-// var closePopupButton = document.querySelector('.buy-tour-popup__close-button');
-//
-// var closePopup = function () {
-//   buyTourPopup.classList.add('hide-popup');
-// };
-//
-// menuButton.classList.add('main-nav__popup-button');
-// menu.classList.add('close-popup');
-//
-// button.onclick = function () {
-//   menu.classList.toggle('close-popup');
-//   button.classList.toggle('main-nav__toggle--closed');
-//   button.classList.toggle('main-nav__toggle--opened');
-//   menuButton.classList.toggle('main-nav__popup-button');
-// };
-//
-// buttonBuyTour.forEach(function (it) {
-//   it.addEventListener('click', function () {
-//     buyTourPopup.classList.remove('hide-popup');
-//   });
-// });
-//
-// closePopupButton.addEventListener('click', closePopup);
-//
-// window.addEventListener('keydown', function (evt) {
-//   if (evt.key === 'Escape') {
-//     evt.preventDefault();
-//     closePopup();
-//   }
-// });
