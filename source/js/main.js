d8c611ff5ae91b20f31a4290c90cf04515b9c74b
pervakam <52745442+pervakam@users.noreply.github.com>
2020-07-01 23:30:05 +0300

    Правки по замечаниям

---
+++ source/js/main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+(function () {
+
+  var button = document.querySelector('.page-header__toggle');
+  var menu = document.querySelector('.page-header__nav-wrapper');
+  var logo = document.querySelector('.page-header__logo-fill');
+  var telField = document.getElementById('tel-field');
+
+  button.classList.add('page-header__toggle--open');
+  menu.classList.add('page-header__closed');
+  menu.classList.add('page-header__nav-modal');
+  logo.classList.add('page-header__logo-fill--white');
+
+  var closeMenu = function () {
+    menu.classList.toggle('page-header__closed');
+    button.classList.toggle('page-header__toggle--close');
+    button.classList.toggle('page-header__toggle--open');
+    logo.classList.toggle('page-header__logo-fill--white');
+  };
+
+  button.addEventListener('click', closeMenu);
+
+  telField.onkeydown = function (evt) {
+    return !(/^[А-Яа-яA-Za-z ]$/.test(evt.key));
+  }
+
+
+})();
