7b24c742d9fa1491b2824c14c0210d6761a7a589
pervakam <52745442+pervakam@users.noreply.github.com>
2020-07-03 11:14:17 +0300

    Завершает проект с учётом замечаний

--- build/js/main.js
+++ build/js/main.js
@@ -7,11 +7,6 @@
   var logo = document.querySelector('.page-header__logo-fill');
   var telField = document.getElementById('tel-field');
 
-  button.classList.add('page-header__toggle--open');
-  menu.classList.add('page-header__closed');
-  menu.classList.add('page-header__nav-modal');
-  logo.classList.add('page-header__logo-fill--white');
-
   var closeMenu = function () {
     menu.classList.toggle('page-header__closed');
     button.classList.toggle('page-header__toggle--close');
@@ -19,11 +14,23 @@
     logo.classList.toggle('page-header__logo-fill--white');
   };
 
-  button.addEventListener('click', closeMenu);
+  var activateMobile = function () {
+    button.classList.add('page-header__toggle--open');
+    menu.classList.add('page-header__closed');
+    menu.classList.add('page-header__nav-modal');
+    logo.classList.add('page-header__logo-fill--white');
 
-  telField.onkeydown = function (evt) {
-    return !(/^[А-Яа-яA-Za-z ]$/.test(evt.key));
+    button.addEventListener('click', closeMenu);
   }
 
+  if (menu) {
+    activateMobile();
+  }
+
+  if (telField) {
+    telField.onkeydown = function (evt) {
+      return !(/^[А-Яа-яA-Za-z ]$/.test(evt.key));
+    }
+  }
 
 })();
